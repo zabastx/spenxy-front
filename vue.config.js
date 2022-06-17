26c672edce3068dbbc9434ef26822f7701f039be
Danil Popov <39392397+zabastx@users.noreply.github.com>
2022-06-17 09:18:17 +0300

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
 	productionSourceMap: false,
-	publicPath: '/front/'
+	publicPath: '/spenxy-front/'
 	// devServer: {
 	// 	proxy: {
 	// 		'^/api': {
